3223008727381c5060a0a99d8eb9ddd5f036d5cf
jvbrink <jonasvdbrink@gmail.com>
2021-10-27 19:56:34 +0200

    La til sortere liste

---
+++ dag2/live-koding/sortere_liste.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+personer = ["Andreas", "Monica", "Sindre", 
+            "Peter", "Silje", "Beate"]
+
+# Sorter lista permanent
+personer.sort()
+
+print("Alfabetisert:")
+print(personer)
+
+print(f"Det er {len(personer)} navn i lista.")
